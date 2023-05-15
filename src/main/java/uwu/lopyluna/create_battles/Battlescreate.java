c31ebcaa63cbe97dbc79d451f73ad07a20791a6d
xuwu / luna <loganalexzand@gmail.com>
2023-05-15 06:59:09 -0800

    yippee

--- src/main/java/uwu/lopyluna/create_battles/Battlescreate.java
+++ src/main/java/uwu/lopyluna/create_battles/Battlescreate.java
@@ -1,39 +1,44 @@
 package uwu.lopyluna.create_battles;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
+import com.simibubi.create.foundation.data.CreateRegistrate;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
+import uwu.lopyluna.create_battles.block.YIPPEE;
+import uwu.lopyluna.create_battles.item.Pipebomb;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(createbattlesyippee.MOD_ID)
-public class createbattlesyippee
+@Mod(Battlescreate.MOD_ID)
+public class Battlescreate
 {
     public static final String MOD_ID = "create_battles";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public createbattlesyippee()
+
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Battlescreate.MOD_ID);
+
+    public Battlescreate()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        Pipebomb.register(eventBus);
+        YIPPEE.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-    }
 
+    }
+    public static CreateRegistrate registrate() {
+        return REGISTRATE;
+    }
     private void setup(final FMLCommonSetupEvent event)
     {
         // some preinit code
---
+++ src/main/java/uwu/lopyluna/create_battles/block/YIPPEE.java
@@ -0,0 +1,61 @@
+package uwu.lopyluna.create_battles.block;
+
+import com.simibubi.create.AllSpriteShifts;
+import com.simibubi.create.content.contraptions.base.CasingBlock;
+import com.simibubi.create.foundation.data.BuilderTransformers;
+import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import uwu.lopyluna.create_battles.Battlescreate;
+import uwu.lopyluna.create_battles.item.Pipebomb;
+import uwu.lopyluna.create_battles.item.PipebombTab;
+import java.util.function.Supplier;
+import static uwu.lopyluna.create_battles.Battlescreate.REGISTRATE;
+
+public class YIPPEE {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, Battlescreate.MOD_ID);
+
+
+    //YIPPEE BLOCK PIPEBOMB YUMMY
+    public static final RegistryObject<Block> mithril_block = registerBlock("mithril_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.NETHERITE_BLOCK).strength(9f,9f).requiresCorrectToolForDrops()), PipebombTab.BattleCreate_TAB);
+    public static final RegistryObject<Block> steel_block = registerBlock("steel_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.NETHERITE_BLOCK).strength(9f,9f).requiresCorrectToolForDrops()), PipebombTab.BattleCreate_TAB);
+
+    public static final BlockEntry<CasingBlock> MITHRIL_CASING = REGISTRATE.block("mithril_casing", CasingBlock::new)
+            .properties(p -> p.color(MaterialColor.TERRACOTTA_BROWN))
+            .transform(BuilderTransformers.casing(() -> AllSpriteShifts.BRASS_CASING))
+            .simpleItem()
+            .register();
+
+
+
+
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                           CreativeModeTab tab) {
+        return Pipebomb.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
