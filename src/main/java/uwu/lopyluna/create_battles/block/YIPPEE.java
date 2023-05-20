49f72491b799b89ab3241a5844da6b5218293cf9
xuwu / luna <loganalexzand@gmail.com>
2023-05-20 06:15:03 -0800

    yippee

--- src/main/java/uwu/lopyluna/create_battles/block/YIPPEE.java
+++ src/main/java/uwu/lopyluna/create_battles/block/YIPPEE.java
@@ -1,12 +1,20 @@
 package uwu.lopyluna.create_battles.block;
 
 import com.simibubi.create.AllSpriteShifts;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.base.CasingBlock;
+import com.simibubi.create.content.contraptions.relays.encased.EncasedCogCTBehaviour;
+import com.simibubi.create.content.contraptions.relays.encased.EncasedCogwheelBlock;
+import com.simibubi.create.content.contraptions.relays.encased.EncasedShaftBlock;
 import com.simibubi.create.foundation.data.BuilderTransformers;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.utility.Couple;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -20,6 +28,9 @@ import uwu.lopyluna.create_battles.Battlescreate;
 import uwu.lopyluna.create_battles.item.Pipebomb;
 import uwu.lopyluna.create_battles.item.PipebombTab;
 import java.util.function.Supplier;
+
+import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
+import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 import static uwu.lopyluna.create_battles.Battlescreate.REGISTRATE;
 
 public class YIPPEE {
@@ -28,18 +39,28 @@ public class YIPPEE {
 
 
     //YIPPEE BLOCK PIPEBOMB YUMMY
-    public static final RegistryObject<Block> mithril_block = registerBlock("mithril_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.NETHERITE_BLOCK).strength(9f,9f).requiresCorrectToolForDrops()), PipebombTab.BattleCreate_TAB);
-    public static final RegistryObject<Block> steel_block = registerBlock("steel_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.NETHERITE_BLOCK).strength(9f,9f).requiresCorrectToolForDrops()), PipebombTab.BattleCreate_TAB);
+    public static final RegistryObject<Block> mithril_block = registerBlock("mithril_block", () -> new Block(BlockBehaviour.Properties
+            .of(Material.METAL).sound(SoundType.NETHERITE_BLOCK)
+            .strength(10f,25f)
+            .requiresCorrectToolForDrops()
+    ), PipebombTab.BattleCreate_TAB);
+    public static final RegistryObject<Block> steel_block = registerBlock("steel_block", () -> new Block(BlockBehaviour.Properties
+            .of(Material.METAL)
+            .sound(SoundType.NETHERITE_BLOCK)
+            .strength(10f,25f)
+            .requiresCorrectToolForDrops()
+    ), PipebombTab.BattleCreate_TAB);
 
 
     //BLOKYENTRY
-    public static final BlockEntry<CasingBlock> MITHRIL_CASING = REGISTRATE.block("mithril_casing", CasingBlock::new).properties(p -> p.color(MaterialColor.TERRACOTTA_CYAN).sound(SoundType.NETHERITE_BLOCK)).transform(BuilderTransformers.casing(() -> YIPPEESpriteShifts.MITHRIL_CASING)).simpleItem().register();
-
-
-
-
-
-
+    public static final BlockEntry<CasingBlock> mithril_casing = REGISTRATE.block("mithril_casing", CasingBlock::new)
+            .properties(p -> p.color(MaterialColor.TERRACOTTA_CYAN))
+            .properties(p -> p.strength(10f,25f))
+            .properties(p -> p.requiresCorrectToolForDrops())
+            .properties(p -> p.sound(SoundType.NETHERITE_BLOCK))
+            .transform(BuilderTransformers.casing(() -> YIPPEESpriteShifts.MITHRIL_CASING))
+            .simpleItem()
+            .register();
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/uwu/lopyluna/create_battles/block/YIPPEESpriteShifts.java
+++ src/main/java/uwu/lopyluna/create_battles/block/YIPPEESpriteShifts.java
@@ -13,11 +13,24 @@ public class YIPPEESpriteShifts {
 
 
 
-    public static final CTSpriteShiftEntry MITHRIL_CASING = omni("mithril_casing");
+    public static final CTSpriteShiftEntry
+            MITHRIL_CASING = omni("mithril_casing"),
+            MITHRIL_ENCASED_COGWHEEL_SIDE = vertical("mithril_encased_cogwheel_side"),
+            MITHRIL_ENCASED_COGWHEEL_OTHERSIDE = horizontal("mithril_encased_cogwheel_side");
+
+
 
 
     private static CTSpriteShiftEntry omni(String name) {
         return getCT(AllCTTypes.OMNIDIRECTIONAL, name);
+    }
+
+    private static CTSpriteShiftEntry horizontal(String name) {
+        return getCT(AllCTTypes.HORIZONTAL, name);
+    }
+
+    private static CTSpriteShiftEntry vertical(String name) {
+        return getCT(AllCTTypes.VERTICAL, name);
     }
     private static CTSpriteShiftEntry getCT(CTType type, String blockTextureName, String connectedTextureName) {
         return CTSpriteShifter.getCT(type, Battlescreate.asResource("block/" + blockTextureName), Battlescreate.asResource("block/" + connectedTextureName + "_connected"));
