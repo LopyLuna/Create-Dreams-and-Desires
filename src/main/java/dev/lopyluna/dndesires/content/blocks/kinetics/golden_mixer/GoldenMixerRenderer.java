0f0b9a39ae6ecc9d54c09fab01fd1ed249a5bf3b
SlyScroll <kylefrodsham@hotmail.co.uk>
2026-03-01 14:00:34 +0000

    Update GoldenMixerRenderer.java

--- src/main/java/dev/lopyluna/dndesires/content/blocks/kinetics/golden_mixer/GoldenMixerRenderer.java
+++ src/main/java/dev/lopyluna/dndesires/content/blocks/kinetics/golden_mixer/GoldenMixerRenderer.java
@@ -1,46 +1,60 @@
 package dev.lopyluna.dndesires.content.blocks.kinetics.golden_mixer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
-import dev.engine_room.flywheel.api.visualization.VisualizationManager;
 import dev.lopyluna.dndesires.register.client.DesiresPartialModels;
+
+import dev.engine_room.flywheel.api.visualization.VisualizationManager;
 import net.createmod.catnip.animation.AnimationTickHolder;
 import net.createmod.catnip.render.CachedBuffers;
+import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class GoldenMixerRenderer extends KineticBlockEntityRenderer<GoldenMixerBE> {
-    public GoldenMixerRenderer(BlockEntityRendererProvider.Context context) {
-        super(context);
-    }
 
-    @Override
-    public boolean shouldRenderOffScreen(@NotNull GoldenMixerBE be) {
-        return true;
-    }
+	public GoldenMixerRenderer(BlockEntityRendererProvider.Context context) {
+		super(context);
+	}
 
-    @Override
-    protected void renderSafe(GoldenMixerBE be, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
-        if (VisualizationManager.supportsVisualization(be.getLevel())) return;
-        var blockState = be.getBlockState();
-        var vb = buffer.getBuffer(RenderType.cutoutMipped());
+	@Override
+	public boolean shouldRenderOffScreen(GoldenMixerBE be) {
+		return true;
+	}
 
-        var superBuffer = CachedBuffers.partial(AllPartialModels.SHAFTLESS_LARGE_COGWHEEL, blockState);
-        standardKineticRotationTransform(superBuffer, be, light).renderInto(ms, vb);
+	@Override
+	protected void renderSafe(GoldenMixerBE be, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
 
-        var renderedHeadOffset = be.getRenderedHeadOffset(partialTicks);
-        var speed = be.getRenderedHeadRotationSpeed();
-        var time = AnimationTickHolder.getRenderTime(be.getLevel());
-        var angle = ((time * speed * 6 / 10f) % 360) / 180 * (float) Math.PI;
+		if (VisualizationManager.supportsVisualization(be.getLevel())) return;
 
-        var poleRender = CachedBuffers.partial(DesiresPartialModels.GOLDEN_MIXER_POLE, blockState);
-        poleRender.translate(0, -renderedHeadOffset, 0).light(light).renderInto(ms, vb);
+		BlockState blockState = be.getBlockState();
 
-        var headRender = CachedBuffers.partial(DesiresPartialModels.GOLDEN_MIXER_HEAD, blockState);
-        headRender.rotateCentered(angle, Direction.UP).translate(0, -renderedHeadOffset, 0).light(light).renderInto(ms, vb);
-    }
+		VertexConsumer vb = buffer.getBuffer(RenderType.solid());
+
+		SuperByteBuffer superBuffer = CachedBuffers.partial(AllPartialModels.SHAFTLESS_LARGE_COGWHEEL, blockState);
+		standardKineticRotationTransform(superBuffer, be, light).renderInto(ms, vb);
+
+		float renderedHeadOffset = be.getRenderedHeadOffset(partialTicks);
+		float speed = be.getRenderedHeadRotationSpeed(partialTicks);
+		float time = AnimationTickHolder.getRenderTime(be.getLevel());
+		float angle = ((time * speed * 6 / 10f) % 360) / 180 * (float) Math.PI;
+
+		SuperByteBuffer poleRender = CachedBuffers.partial(AllPartialModels.MECHANICAL_MIXER_POLE, blockState);
+		poleRender.translate(0, -renderedHeadOffset, 0)
+				.light(light)
+				.renderInto(ms, vb);
+
+		VertexConsumer vbCutout = buffer.getBuffer(RenderType.cutoutMipped());
+		SuperByteBuffer headRender = CachedBuffers.partial(AllPartialModels.MECHANICAL_MIXER_HEAD, blockState);
+		headRender.rotateCentered(angle, Direction.UP)
+				.translate(0, -renderedHeadOffset, 0)
+				.light(light)
+				.renderInto(ms, vbCutout);
+	}
+
 }
