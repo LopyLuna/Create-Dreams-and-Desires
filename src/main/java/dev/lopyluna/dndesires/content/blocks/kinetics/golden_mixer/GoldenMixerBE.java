1edc69446a253e79cc622fca3905df0219d68d3b
SlyScroll <kylefrodsham@hotmail.co.uk>
2026-03-01 14:26:21 +0000

    Update GoldenMixerBE.java

--- src/main/java/dev/lopyluna/dndesires/content/blocks/kinetics/golden_mixer/GoldenMixerBE.java
+++ src/main/java/dev/lopyluna/dndesires/content/blocks/kinetics/golden_mixer/GoldenMixerBE.java
@@ -146,7 +146,7 @@ public class GoldenMixerBE extends BasinOperatingBlockEntity {
 
 			if ((!level.isClientSide || isVirtual()) && runningTicks == 20) {
 				if (processingTicks < 0) {
-					float recipeSpeed = 3;
+					float recipeSpeed = 0.33;
 					if (currentRecipe instanceof StandardProcessingRecipe) {
 						int t = ((StandardProcessingRecipe<?>) currentRecipe).getProcessingDuration();
 						if (t != 0)
