0f4c7fe711ce65f0c41bab856399cd60f9256316
Brotality <8380331+BrotalityG@users.noreply.github.com>
2026-06-20 13:32:39 -0400

    Update BoreBlock.java
    Fixed Bore blocks to have full collision with players and entities but have reduced size hitboxes on moving contraptions to aid in block breaking and prevent issues with aeronautics contraptions being flung while mining with them

--- src/main/java/dev/lopyluna/dndesires/content/blocks/logistics/bore_block/BoreBlock.java
+++ src/main/java/dev/lopyluna/dndesires/content/blocks/logistics/bore_block/BoreBlock.java
@@ -8,23 +8,36 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import dev.ryanhcode.sable.api.block.BlockSubLevelCollisionShape;
 
 @SuppressWarnings("NullableProblems")
-public class BoreBlock extends Block {
+public class BoreBlock extends Block implements BlockSubLevelCollisionShape {
+    private static final VoxelShape worldCollision = Shapes.block();
+    private static final VoxelShape mountCollision = Block.box(7, 7, 7, 9, 9, 9);
+
     public BoreBlock(Properties properties) {
         super(properties);
     }
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return context.equals(CollisionContext.empty()) ? Block.box(4, 4, 4, 12, 12, 12) : super.getShape(state, level, pos, context);
+        return worldCollision;
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return worldCollision;
+    }
+
+    @Override
+    public VoxelShape getSubLevelCollisionShape(BlockGetter blockGetter, BlockState state) {
+        return mountCollision;
     }
 
     @Override
     protected VoxelShape getVisualShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return Shapes.block();
     }
-
 
     @Override
     protected boolean skipRendering(BlockState state, BlockState adjacentBlockState, Direction side) {
