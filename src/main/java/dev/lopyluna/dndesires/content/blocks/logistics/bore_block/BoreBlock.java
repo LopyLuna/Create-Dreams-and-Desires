a5a6e28dd1920242fcf75a53fe5c0144513d8154
Brotality <8380331+BrotalityG@users.noreply.github.com>
2026-06-21 00:08:57 -0400

    Fixed for optional sable compat
    Defaults to already existing bore block behavior unless Sable is installed, where instead the new collisions apply.

--- src/main/java/dev/lopyluna/dndesires/content/blocks/logistics/bore_block/BoreBlock.java
+++ src/main/java/dev/lopyluna/dndesires/content/blocks/logistics/bore_block/BoreBlock.java
@@ -8,30 +8,16 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import dev.ryanhcode.sable.api.block.BlockSubLevelCollisionShape;
 
 @SuppressWarnings("NullableProblems")
-public class BoreBlock extends Block implements BlockSubLevelCollisionShape {
-    private static final VoxelShape worldCollision = Shapes.block();
-    private static final VoxelShape mountCollision = Block.box(7, 7, 7, 9, 9, 9);
-
+public class BoreBlock extends Block {
     public BoreBlock(Properties properties) {
         super(properties);
     }
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return worldCollision;
-    }
-
-    @Override
-    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return worldCollision;
-    }
-
-    @Override
-    public VoxelShape getSubLevelCollisionShape(BlockGetter blockGetter, BlockState state) {
-        return mountCollision;
+        return context.equals(CollisionContext.empty()) ? Block.box(4, 4, 4, 12, 12, 12) : super.getShape(state, level, pos, context);
     }
 
     @Override
