e1ba270d7a0ce53f49629126593bffa897201723
Niklas Kovalainen <34051683+KoalaNalle@users.noreply.github.com>
2026-07-05 14:27:14 +0300

    Update DServer.java
    Config for Handheldsaw air usage multiplier

--- src/main/java/dev/lopyluna/dndesires/content/configs/DServer.java
+++ src/main/java/dev/lopyluna/dndesires/content/configs/DServer.java
@@ -9,7 +9,7 @@ public class DServer extends ConfigBase {
             "server", "Configs for the World");
     public final ConfigInt chanceForOreStone = i(25, 1, 100, "chanceForOreStone", Comments.chanceForOreStone);
     public final ConfigInt chanceForArtificialOreStone = i(5, 0, 100, "chanceForArtificialOreStone", Comments.chanceForArtificialOreStone);
-
+    public final ConfigInt handheldSawAirUsageMultiplier = i(4, 1, 64, "handheldSawAirUsageMultiplier", Comments.handheldSawAirUsageMultiplier);
 
     public final DKinetics kinetics = nested(0, DKinetics::new, "Parameters and abilities of Create: Desires 2 Dream's kinetic mechanisms");
 
@@ -21,5 +21,6 @@ public class DServer extends ConfigBase {
     private static class Comments {
         static String chanceForOreStone = "Chance for and Ore Stone to spawn when on top of Bedrock while Milkshake Stone Generating";
         static String chanceForArtificialOreStone = "Chance for and Ore Stone to spawn when on top of Artificial Bedrock while Milkshake Stone Generating";
+        static String handheldSawAirUsageMultiplier = "Multiplier for how many uses the Handheld Saw gets from a Backtank. Higher values make the saw consume less air. Default 4 makes it usually consume 1 air per use";
     }
 }
